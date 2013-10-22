a938a2f060c1fc678f026560a424a99672c45ec7
Jacob Roberts <jacobr@marlboro.edu>
2013-10-21 20:11:29 -0400

    occupied method test

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestremovePieceFromBoard(t *testing.T) {
+func TestRemovePieceFromBoard(t *testing.T) {
 	in := Board{
 		Board: []Piece{
 			Piece{
@@ -33,5 +33,38 @@ func TestremovePieceFromBoard(t *testing.T) {
 		if p.Name != out.Board[i].Name {
 			t.Errorf("removePieceFromBoard failure: was expecting piece name %s, got %s", out.Board[i].Name, p.Name)
 		}
+	}
+}
+
+func TestOccupied(t *testing.T) {
+	b := &Board{}
+	b.SetUpPieces()
+	whitesquare := &Square{
+		X: 1,
+		Y: 1,
+	}
+	blacksquare := &Square{
+		X: 8,
+		Y: 8,
+	}
+	emptysquare := &Square{
+		X: 5,
+		Y: 5,
+	}
+	nonsquare := &Square{
+		X: 10,
+		Y: 10,
+	}
+	if out := b.occupied(whitesquare); out != 1 {
+		t.Errorf("expected 1, got %d", out)
+	}
+	if out := b.occupied(blacksquare); out != -1 {
+		t.Errorf("expected -1, got %d", out)
+	}
+	if out := b.occupied(emptysquare); out != 0 {
+		t.Errorf("expected 0, got %d", out)
+	}
+	if out := b.occupied(nonsquare); out != -2 {
+		t.Errorf("expected -2, got %d", out)
 	}
 }
