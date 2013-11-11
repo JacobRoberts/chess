04148e5fb2016664c292996a230f9c2cb7e8b547
Jacob Roberts <jacobr@marlboro.edu>
2013-11-10 21:33:18 -0500

    negamax able to find two move checkmate
    its pretty slow though...

--- negamax/negamax_test.go
+++ negamax/negamax_test.go
@@ -70,3 +70,66 @@ func TestOneMoveCheckmate(t *testing.T) {
 		t.Error("Negmax could not find one move checkmate. Returned a move of ", move)
 	}
 }
+
+func TestTwoMoveCheckmate(t *testing.T) {
+	board := &engine.Board{
+		Board: []engine.Piece{
+			engine.Piece{
+				Name: "k",
+				Position: engine.Square{
+					X: 2,
+					Y: 1,
+				},
+				Color: -1,
+				Directions: [][2]int{
+					{1, 1},
+					{1, 0},
+					{1, -1},
+					{0, 1},
+					{0, -1},
+					{-1, 1},
+					{-1, 0},
+					{-1, -1},
+				},
+			},
+			engine.Piece{
+				Name: "r",
+				Position: engine.Square{
+					X: 3,
+					Y: 7,
+				},
+				Color: 1,
+				Directions: [][2]int{
+					{1, 0},
+					{-1, 0},
+					{0, 1},
+					{0, -1},
+				},
+				Infinite_direction: true,
+			},
+			engine.Piece{
+				Name: "r",
+				Position: engine.Square{
+					X: 4,
+					Y: 8,
+				},
+				Color: 1,
+				Directions: [][2]int{
+					{1, 0},
+					{-1, 0},
+					{0, 1},
+					{0, -1},
+				},
+				Infinite_direction: true,
+			},
+		},
+		Turn: 1,
+	}
+	move := NegaMax(board, 4)
+	if err := board.Move(move); err != nil {
+		t.Error("Move from negamax was rejected by board.Move() because ", err)
+	}
+	if board.IsCheck(-1) == false {
+		t.Error("Negmax could not find two move checkmate. Returned a move of ", move)
+	}
+}
