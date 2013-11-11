07f444d790cf74b4390ab834802ff01ae5cb727f
Jacob Roberts <jacobr@marlboro.edu>
2013-11-10 20:43:00 -0500

    basic game over support

--- negamax/evaluate.go
+++ negamax/evaluate.go
@@ -5,5 +5,12 @@ import (
 )
 
 func EvalBoard(b *engine.Board) float64 {
+	if over := b.IsOver(); over != 0 {
+		if over == 1 {
+			return 0
+		} else {
+			return float64(498 * over * b.Turn)
+		}
+	}
 	return 0
 }
