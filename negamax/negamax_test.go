1e63131461098d016d20a8f7ec331d9b08f67b4f
Jacob Roberts <jacobr@marlboro.edu>
2013-11-11 02:28:03 -0500

    Better testing. Negamax does not pass testing.

--- negamax/negamax_test.go
+++ negamax/negamax_test.go
@@ -129,7 +129,7 @@ func TestTwoMoveCheckmate(t *testing.T) {
 	if err := board.Move(move); err != nil {
 		t.Error("Move from negamax was rejected by board.Move() because ", err)
 	}
-	if board.IsCheck(-1) == false {
+	if move.Begin.X != 4 || move.End.X != 2 || move.End.Y != 7 {
 		t.Error("Negmax could not find two move checkmate. Returned a move of ", move)
 	}
 }
