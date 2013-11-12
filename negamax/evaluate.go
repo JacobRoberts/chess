1f197ae7bba3cbc929a48912840fd3e2429b672c
Jacob Roberts <jacobr@marlboro.edu>
2013-11-12 12:03:37 -0500

    docs

--- negamax/evaluate.go
+++ negamax/evaluate.go
@@ -4,6 +4,8 @@ import (
 	"chess/engine"
 )
 
+// Returns the score from the point of view of the person whose turn it is.
+// Positive numbers indicate a stronger position.
 func EvalBoard(b *engine.Board) float64 {
 	if over := b.IsOver(); over != 0 {
 		if over == 1 {
