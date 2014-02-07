dde6c0aa8c89350c215300d90fceca7b4f6f165a
Jacob Roberts <jacobr@marlboro.edu>
2014-02-06 20:50:22 -0500

    kings at a fixed spot in board to reduce loops through board

--- engine/board.go
+++ engine/board.go
@@ -1,9 +1,5 @@
 package engine
 
-import (
-	"fmt"
-)
-
 // array of all pieces on a given board
 type Board struct {
 	Board    []*Piece // all of the pieces on the board
@@ -16,11 +12,10 @@ type Board struct {
 // Returns true if king in check / false if not.
 func (b *Board) IsCheck(color int) bool {
 	var kingsquare Square
-	for _, piece := range b.Board {
-		if piece.Name == 'k' && piece.Color == color {
-			kingsquare = piece.Position
-			break
-		}
+	if color == 1 {
+		kingsquare = b.Board[0].Position
+	} else if color == -1 {
+		kingsquare = b.Board[1].Position
 	}
 	for _, piece := range b.Board {
 		if piece.Color == color*-1 {
@@ -77,11 +72,10 @@ func (b *Board) NewGen() []*Board {
 // Returns 2 if white wins, -2 if black wins, 1 if it's stalemate, 0 if the game is still going.
 func (b *Board) IsOver() int {
 	var kingindex int
-	for i, p := range b.Board {
-		if p.Name == 'k' && p.Color == b.Turn {
-			kingindex = i
-			break
-		}
+	if b.Turn == 1 {
+		kingindex = 0
+	} else if b.Turn == -1 {
+		kingindex = 1
 	}
 	if len(b.Board[kingindex].legalMoves(b, true)) == 0 {
 		if b.IsCheck(b.Turn) {
@@ -94,61 +88,46 @@ func (b *Board) IsOver() int {
 	return 0
 }
 
-// Prints the board to the console in a human-readable format.
-func (b *Board) PrintBoard() {
-	boardarr := [8][8]byte{}
-	for _, piece := range b.Board {
-		if piece.Position.X != 0 {
-			boardarr[piece.Position.Y-1][piece.Position.X-1] = piece.Name
-		}
-	}
-	for y := 7; y >= 0; y-- {
-		for x := 0; x < 8; x++ {
-			if piece := boardarr[y][x]; piece != 'q' && piece != 'k' && piece != 'b' && piece != 'n' || piece != 'p' && piece != 'r' {
-				fmt.Printf("  ")
-			} else {
-				fmt.Printf("%s ", boardarr[y][x])
-			}
-		}
-		fmt.Println()
-	}
-}
-
 // Resets a given board to its starting position.
 func (b *Board) SetUpPieces() {
 	// for readability, this should be the last function in the file
 	b.Board = make([]*Piece, 0)
 	pawnrows := [2]int{2, 7}
-	for _, rank := range pawnrows {
-		var color int
-		if rank == 2 {
-			color = 1
-		} else {
-			color = -1
-		}
-		for file := 1; file <= 8; file++ {
-			piece := Piece{
-				Name: 'p',
-				Position: Square{
-					X: file,
-					Y: rank,
-				},
-				Color:           color,
-				Can_double_move: true,
-				Directions: [][2]int{
-					{0, 1 * color},
-				},
-				Value: 1,
-			}
-			b.Board = append(b.Board, &piece)
-		}
-	}
 	piecerows := [2]int{1, 8}
 	rookfiles := [2]int{1, 8}
 	knightfiles := [2]int{2, 7}
 	bishopfiles := [2]int{3, 6}
 	queenfile := 4
 	kingfile := 5
+	for _, rank := range piecerows {
+		// put the kings in first
+		var color int
+		if rank == 1 {
+			color = 1
+		} else {
+			color = -1
+		}
+		king := Piece{
+			Name: 'k',
+			Position: Square{
+				X: kingfile,
+				Y: rank,
+			},
+			Color: color,
+			Directions: [][2]int{
+				{1, 1},
+				{1, 0},
+				{1, -1},
+				{0, 1},
+				{0, -1},
+				{-1, 1},
+				{-1, 0},
+				{-1, -1},
+			},
+			Can_castle: true,
+		}
+		b.Board = append(b.Board, &king)
+	}
 	for _, rank := range piecerows {
 		var color int
 		if rank == 1 {
@@ -238,25 +217,29 @@ func (b *Board) SetUpPieces() {
 			Infinite_direction: true,
 		}
 		b.Board = append(b.Board, &queen)
-		king := Piece{
-			Name: 'k',
-			Position: Square{
-				X: kingfile,
-				Y: rank,
-			},
-			Color: color,
-			Directions: [][2]int{
-				{1, 1},
-				{1, 0},
-				{1, -1},
-				{0, 1},
-				{0, -1},
-				{-1, 1},
-				{-1, 0},
-				{-1, -1},
-			},
-			Can_castle: true,
+	}
+	for _, rank := range pawnrows {
+		var color int
+		if rank == 2 {
+			color = 1
+		} else {
+			color = -1
 		}
-		b.Board = append(b.Board, &king)
+		for file := 1; file <= 8; file++ {
+			piece := Piece{
+				Name: 'p',
+				Position: Square{
+					X: file,
+					Y: rank,
+				},
+				Color:           color,
+				Can_double_move: true,
+				Directions: [][2]int{
+					{0, 1 * color},
+				},
+				Value: 1,
+			}
+			b.Board = append(b.Board, &piece)
+		}
 	}
 }
