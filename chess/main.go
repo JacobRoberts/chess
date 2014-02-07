b8cae26799fa47121d5723e6bfe7b1d03b4aad17
Jacob Roberts <jacobr@marlboro.edu>
2014-02-06 20:50:30 -0500

    minor edits

--- chess/main.go
+++ chess/main.go
@@ -1,10 +1,13 @@
 package main
 
+import (
+	"chess/engine"
+)
+
 // handles user interface
 func main() {
 	board := &engine.Board{Turn: 1}
 	board.SetUpPieces()
-	board.PrintBoard()
 	color_names := make(map[int]string)
 	color_names[1], color_names[-1] = "White", "Black"
 	for {
