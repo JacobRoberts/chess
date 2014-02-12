19b56392a5d552acb30973197eaafcd36ea9885e
Jacob Roberts <jacobr@marlboro.edu>
2014-02-11 21:15:34 -0500

    initial testing

--- negamax/evaluate_test.go
+++ negamax/evaluate_test.go
@@ -1,9 +1,17 @@
 package negamax
 
 import (
+	"chess/engine"
 	"testing"
 )
 
 func TestAttackArray(t *testing.T) {
-
+	board := &engine.Board{Turn: 1}
+	board.PlacePiece('k', 1, 2, 2)
+	board.PlacePiece('r', -1, 1, 8)
+	board.PlacePiece('r', -1, 8, 1)
+	attackarray := createAttackArray(board)
+	if attackarray[0][0] != -1 {
+		t.Errorf("Expected -1 attack value on square (1, 1), got %d", attackarray[0][0])
+	}
 }
