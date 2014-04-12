dd894af6e3f6d90e87930ab4d3b7267ddbba89ea
Jacob Roberts <jacobr@marlboro.edu>
2014-04-11 23:05:02 -0400

    better naming

--- negamax/negamax_test.go
+++ negamax/negamax_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	search_functions := []func(*engine.Board, int, float64, float64) *engine.Move{AlphaBeta}
-	search_strings := []string{"AlphaBeta"}
+	functions := []func(*engine.Board, int, float64, float64) *engine.Move{AlphaBeta}
+	function_names := []string{"AlphaBeta"}
 	board := &engine.Board{Turn: 1}
 	board.PlacePiece('k', 1, 1, 3)
 	board.PlacePiece('k', -1, 1, 1)
 	board.PlacePiece('r', 1, 3, 3)
-	for i, f := range search_functions {
+	for i, f := range functions {
 		move := f(board, 4, BLACKWIN*2, WHITEWIN*2)
 		if move.Begin.X != 3 || move.End.Y != 1 {
-			t.Errorf("\nFunction %s gave move %s when Rc3-c1 was expected\n Unable to solve one move checkmate", search_strings[i], move.ToString())
+			t.Errorf("\nFunction %s gave move %s when Rc3-c1 was expected\n Unable to solve one move checkmate", function_names[i], move.ToString())
 		}
 		if move.Score != WHITEWIN {
 			t.Errorf("Checkmate should have given score %f, instead gave score %f", WHITEWIN, move.Score)
@@ -28,10 +28,10 @@ func TestSearch(t *testing.T) {
 	board.PlacePiece('k', 1, 2, 1)
 	board.PlacePiece('r', -1, 3, 7)
 	board.PlacePiece('r', -1, 4, 8)
-	for i, f := range search_functions {
+	for i, f := range functions {
 		move := f(board, 4, BLACKWIN*2, WHITEWIN*2)
 		if move.Begin.X != 4 || move.End.X != 2 || move.End.Y != 8 {
-			t.Errorf("\nFunction %s gave move %s when Rd8-b8 was expected\n Unable to solve two move checkmate", search_strings[i], move.ToString())
+			t.Errorf("\nFunction %s gave move %s when Rd8-b8 was expected\n Unable to solve two move checkmate", function_names[i], move.ToString())
 		}
 		if move.Score != BLACKWIN {
 			t.Errorf("Checkmate should have given score %f, instead gave score %f", BLACKWIN, move.Score)
