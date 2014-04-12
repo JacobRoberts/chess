7f4cf82abc97a14671c85ecadf246246f9d1733a
Jacob Roberts <jacobr@marlboro.edu>
2014-04-11 23:10:06 -0400

    more verbose error

--- negamax/negamax_test.go
+++ negamax/negamax_test.go
@@ -14,9 +14,9 @@ func TestSearch(t *testing.T) {
 	board.PlacePiece('k', -1, 1, 1)
 	board.PlacePiece('r', 1, 3, 3)
 	for i, f := range functions {
-		move := f(board, 4, BLACKWIN*2, WHITEWIN*2)
+		move := f(board, 2, BLACKWIN*2, WHITEWIN*2)
 		if move.Begin.X != 3 || move.End.Y != 1 {
-			t.Errorf("\nFunction %s gave move %s when Rc3-c1 was expected\n Unable to solve one move checkmate", function_names[i], move.ToString())
+			t.Errorf("\nFunction %s gave move %s when Rc3-c1 was expected\n Unable to solve one move checkmate\n Full returned move: %+v", function_names[i], move.ToString(), move)
 		}
 		if move.Score != WHITEWIN {
 			t.Errorf("Checkmate should have given score %f, instead gave score %f", WHITEWIN, move.Score)
@@ -31,7 +31,7 @@ func TestSearch(t *testing.T) {
 	for i, f := range functions {
 		move := f(board, 4, BLACKWIN*2, WHITEWIN*2)
 		if move.Begin.X != 4 || move.End.X != 2 || move.End.Y != 8 {
-			t.Errorf("\nFunction %s gave move %s when Rd8-b8 was expected\n Unable to solve two move checkmate", function_names[i], move.ToString())
+			t.Errorf("\nFunction %s gave move %s when Rd8-b8 was expected\n Unable to solve two move checkmate\n Full returned move: %+v", function_names[i], move.ToString(), move)
 		}
 		if move.Score != BLACKWIN {
 			t.Errorf("Checkmate should have given score %f, instead gave score %f", BLACKWIN, move.Score)
